80c5852c9f9140bbac17b8ad281209694fdfa15c
Michael Schulz <mi.schulz@hotmail.com>
2022-09-06 15:46:26 +0200

    tried to implement stretch todos

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import chalk from 'chalk';
+import test from 'node:test';
 import randomColor from 'randomcolor';
+// import readline from 'readline';
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
 let pattern = '';
 let height = 9;
 let width = 31;
@@ -10,15 +18,34 @@ let color = randomColor({
   hue: 'random',
 });
 
-const chosenColorByUser = args[0];
-const chosenLuminosityByUser = args[1];
+let chosenColorByUser = args[0];
+let chosenLuminosityByUser = args[1];
 
-if (process.argv[2]) {
+if (args[0]) {
   color = randomColor({
-    luminosity: process.argv[3] ? chosenLuminosityByUser : 'dark',
+    luminosity: args[1] ? chosenLuminosityByUser : 'random',
     hue: chosenColorByUser,
   });
 }
+
+// if (args[0].includes('x')) {
+//   const splittedString = args[0].split('x');
+//   width = splittedString[0];
+//   height = splittedString[1];
+
+//   chosenColorByUser = args[1];
+//   chosenLuminosityByUser = args[2];
+// }
+
+// if (args[0] === 'ask') {
+//   rl.question('What color do you want?', (color) => {
+//     rl.question('And what luminosity?', (luminosity) => {
+//       chosenColorByUser = color;
+//       chosenLuminosityByUser = luminosity;
+//       rl.close();
+//     });
+//   });
+// }
 
 for (let i = 1; i <= height; i++) {
   if (i < Math.ceil(height / 2) - 1 || i > Math.ceil(height / 2) + 1) {
